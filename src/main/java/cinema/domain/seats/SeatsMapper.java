e9047bfa676032b3a3843eedfe566a01cf31f58f
Przemek Sipta <przemek.sipta@gmail.com>
2024-11-14 19:30:59 +0100

    test: SeatsControllerTest unit tests

---
+++ src/main/java/cinema/domain/seats/SeatsMapper.java
@@ -0,0 +1,8 @@
+package cinema.domain.seats;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SeatsMapper {
+
+}
---
+++ src/test/java/cinema/rest/seats/SeatsControllerTest.java
@@ -0,0 +1,53 @@
+package cinema.rest.seats;
+
+import cinema.domain.seats.Seat;
+import cinema.domain.seats.SeatsService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SeatsController.class)
+class SeatsControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    SeatsService seatsService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void getSeats_shouldReturnSeats() throws Exception {
+        Seat seat = new Seat();
+        seat.setRow(1);
+        seat.setColumn(1);
+        Seat seat2 = new Seat();
+        seat2.setRow(2);
+        seat2.setColumn(2);
+        List<Seat> seats = List.of(seat, seat2);
+
+        Mockito.when(seatsService.getAllSeats()).thenReturn(seats);
+
+        mvc.perform(get("/seats"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.seats[0].row").value(seat.getRow()))
+                .andExpect(jsonPath("$.seats[0].column").value(seat.getColumn()))
+                .andExpect(jsonPath("$.seats[1].row").value(seat2.getRow()))
+                .andExpect(jsonPath("$.seats[1].column").value(seat2.getColumn()))
+                .andDo(print());
+    }
+}
